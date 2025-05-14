d54769d6108cc89faef4533e5ab7792ccb296093
NandhiEsswaran-001 <nandhiesswaransaravanan@gmail.com>
2025-05-14 21:50:25 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,96 @@
+
+"""
+# displaying text susee
+print("Susee")
+
+# displaying text mahesh
+print("Mahesh")
+
+# displaying text nandhi
+print("Nandhi")
+
+# displaying text god
+print("God")
+"""
+
+"""
+
+city = "Namakkal"
+print(type(city))
+
+age = 17
+print(type(age))
+
+percentage = 67.5
+print(type(percentage))
+
+online = False
+print("Is sanjuv anna online? ", online)
+
+"""
+
+# operators
+# math operators
+"""
+    +, -, *, /, %, **
+    print(10 + 10)
+    print(10 - 5)
+    print(4 * 2)
+    print(4 / 2)  # => decimal
+    print(4 // 2)
+    print(3 ** 2)
+"""
+
+# assignment operators
+"""
+number = 10
+number **= 5
+print(number)
+"""
+#
+# # comparison operators
+# number1 = 5
+# number2 = 2
+#
+# print(number1 > number2) # => returns boolean values
+# print(number1 < number2)
+# print(number1 == number2)
+# print(number1 != number2)
+# print(number1 >= number2)
+# print(number1 <= number2)
+# # return - everything in python returns something
+
+# logical operators
+
+# and, or, not
+
+"""
+
+expression_1 = 5 > 1 # => True
+expression_2 = 101 < 10 # => True
+
+result_1 = expression_1 and expression_2 # => true if both conditions are true
+result_2 = expression_1 or expression_2 # => true if either one of the conditions is true
+
+print(not result_1)
+print(not result_2)
+
+"""
+
+# is, in - specially created for python
+
+# username = "@john"
+
+# result = "j" in username
+# print(result)
+
+# new_user = "@john"
+# print(username is new_user)
+
+
+# homework
+
+# name
+# age
+# display text
+# ouput -> Hi I am (name) and I am (age).
