696b64ad5c2b343c08118c9c577c4acc138a1e23
NandhiEsswaran-001 <nandhiesswaransaravanan@gmail.com>
2025-05-15 21:47:52 +0530

    commit - 2

---
+++ demo_2.py
@@ -0,0 +1,125 @@
+# strings
+
+"""
+string_1 = "Guns"
+string_2 = 'Bullets'
+
+print(string_1 + " " + string_2)
+
+"""
+from os import remove
+
+# indexing
+# name = "Dhoni"
+# print(name[-2])
+
+# slicing
+"""
+simple_sentence = "Hi I am Nandhi"
+# print(simple_sentence[3:7]) # start: stop: step
+# print(simple_sentence[8::2])
+
+
+print(simple_sentence)
+print(simple_sentence[:])
+
+# palindrome
+# malayalam = "malayalam"
+# print(malayalam[::-1])
+
+# print(simple_sentence[::-1])
+numbers = "123456789"
+print(numbers[::-4])
+
+"""
+
+# strings methods
+# print("  sunny leone  ")
+# print("  sunny leone  ".rstrip())
+
+# escape characters
+# """
+# print("Hi \nSabari \"chellam\"")
+#
+# # computer folder
+# print(r'C:\downloads\sujith-nudes\nudes.jpg') # raw string
+#
+# """
+
+# type conversion
+
+
+"""
+number = 10
+print(type(number))
+change_number_into_string = str(number) # global functions
+print(change_number_into_string)
+print(type(change_number_into_string))
+
+string_1 = "10"
+print(type(int(string_1)))
+
+number_2 = 10.20
+print(int(number_2))
+
+"""
+
+# 10 / 2 => decimal values
+# 10 // 2 => truncated value
+
+# input() function
+# instagram login
+
+# username = input("Type your username: ")
+# print("Hello " + username)
+#
+# number = float(input("Enter number: "))
+# print(number * 2)
+
+# banking
+#
+# interest_rate = float(input("Interest amount: "))
+# print(interest_rate)
+# print(type(interest_rate))
+
+
+# lists
+"""
+members = ["Son", "Keerthi", "Mapla Susee", "Sokku Sabari", "Vedapu Mahesh", "HOD", "Fluffy", "Sura sk", "Kutku"]
+
+print(members[-1])
+
+print(members[::-1]) # reversing
+
+members[3] = "Mr.Nandhi"
+
+print(members)
+
+remove_name = members[-1]
+
+members.remove(remove_name)
+
+print(members)
+
+members.append("Auto harish")
+members += ["Kutku", "TDK", "Boost Prasad"]
+
+print(members)
+
+members[3:8] = []
+
+print(members)
+"""
+
+
+# homework - 1
+# get input and store it in a variables
+# add the input to the list dynamically from input
+
+# homework - 2
+# jumble words
+# get a input string
+# reverse it and add them into a list
+# then also reverse that list
+
+
